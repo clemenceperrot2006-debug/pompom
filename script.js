a56f8b9a61d69ddfa50e7700c0823f0b31a04db6
clemenceperrot2006-debug <clemence.perrot2006@gmail.com>
2025-11-24 14:25:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-// Apparition douce au scroll
+// Apparition douce des sections
 const sections = document.querySelectorAll("section");
 window.addEventListener("scroll", () => {
   const trigger = window.innerHeight * 0.85;
   sections.forEach(sec => {
     const top = sec.getBoundingClientRect().top;
     if (top < trigger) sec.classList.add("visible");
+  });
+});
+
+// Apparition des nuages un par un
+const clouds = document.querySelectorAll("#team-activities .cloud");
+window.addEventListener("scroll", () => {
+  const trigger = window.innerHeight * 0.9;
+  clouds.forEach(cloud => {
+    const top = cloud.getBoundingClientRect().top;
+    if (top < trigger) cloud.classList.add("visible");
   });
 });
